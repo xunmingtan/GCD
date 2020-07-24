8384b237273ec41c139e3d0e8500d9ffcab7a7ae
xunming Tan <xunmingtan@qq.com>
2020-07-24 13:46:32 +0800

    第一次提交GCD用法

---
+++ thread/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  thread
+//
+//  Created by xunming Tan on 2020/7/21.
+//  Copyright © 2020 xmt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ thread/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  thread
+//
+//  Created by xunming Tan on 2020/7/21.
+//  Copyright © 2020 xmt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
